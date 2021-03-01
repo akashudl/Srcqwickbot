bc22dd1f9d98f673c8207a75d1fe8e39bec73e96
akashudl <78961715+akashudl@users.noreply.github.com>
2021-03-01 11:51:43 +0530

    Update AuthorizedLoging.jsx

--- src/Components/AuthorizedLoging.jsx
+++ src/Components/AuthorizedLoging.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function AUthorizedLoging() {
     return (
-        <div style={{textAlign:"center"},{color:"white"}}><b>You are Not Authorized User.Please Sign with Qwiksilver mail-id </b></div>
+        <div style={{textAlign:"center"},{color:"blue"}}><b>You are Not Authorized User.Please Sign with Qwiksilver mail-id </b></div>
     );
 }
